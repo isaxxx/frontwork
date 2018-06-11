093ccd17b869c630422b2f319af15aa00959cda3
Masahide Isaka <a1132666@gmail.com>
2018-06-12 02:39:47 +0900

    Add: test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const frontwork = require('../index');
 const test = require('ava');
 
-test('test', (t) => {
+test('test sample', (t) => {
   return new Promise((resolve, reject) => {
 		return resolve(t.pass());
 	});
