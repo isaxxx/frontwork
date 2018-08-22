8d0defd277d3153e6ddcd9fdfab2e10cfd5a03e3
Masahide Isaka <a1132666@gmail.com>
2018-08-22 12:43:59 +0900

    Fix: gitignore and npmignore

--- src/js/app/app.js
+++
@@ -1,7 +0,0 @@
-import Frontwork from 'frontwork/all';
-
-Frontwork.Utility.addBodyClass();
-
-document.addEventListener('DOMContentLoaded', () => {
-  new Frontwork.Menu(1);
-});
--- src/templates/styleguide/assets/styleguide.js
+++
@@ -1,5 +0,0 @@
-import Frontwork from 'frontwork/all';
-
-document.addEventListener('DOMContentLoaded', () => {
-  new Frontwork.Menu(1);
-});
